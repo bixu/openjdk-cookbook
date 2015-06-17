09606fce113e72409c1907d963f3967914850775
Blake Irvin <blake.irvin@yetu.com>
2015-06-17 17:59:57 +0200

    minor fixes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'blake.irvin@gmail.com'
 license 'mit'
 description 'Installs/Configures openjdk'
 long_description 'Installs/Configures openjdk'
-version '0.1.0'
+version '0.1.1'
 
 depends 'apt'
 
